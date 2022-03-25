5e3635d7c613365b79e3c76495beae36022cc5f6
GriffinsBotball <99513783+GriffinsBotball@users.noreply.github.com>
2022-03-25 16:26:33 -0500

    Update Library.h (Roomba)

--- Library.h
+++ Library.h
@@ -1,8 +1,9 @@
-
 void Right_Turn_Roomba();
 void Left_Turn_Roomba();
 void Bump_Square_Up();
 void Insult();
 void Compliment();
 void Button_Square_Up();
-
+void Half_Left_Turn_Roomba();
+void Half_Right_Turn_Roomba();
+void line_follow();
