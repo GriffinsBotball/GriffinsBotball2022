542413d66a7dcb71f269eedb0d2e830a1c76ba7d
GriffinsBotball <99513783+GriffinsBotball@users.noreply.github.com>
2022-03-25 16:14:26 -0500

    Update Initial Ring Manuever.c

--- Ring Initial Maneuver.c
+++ Ring Initial Maneuver.c
@@ -3,11 +3,19 @@
 
 int main()
 {
+    
+    
+    //wait_for_light(5); // Wait For Light
+    
+    shut_down_in(115);
+    
+    msleep(8000);
+    
     enable_servos();
     set_servo_position(2, 50); // Open Claw
     msleep(500);
-
-    set_servo_position(3, 850);// Lower Arm
+    //pop dad dal ope dop  yip did ali pdi pip sni pid ali pid ipe
+    set_servo_position(3, 950);// Lower Arm
     msleep(500);
 
 
@@ -32,6 +40,8 @@ int main()
     motor(1, 50);
     msleep(2000);
     
+    F1();
+    
     B12();
     
     ao();
@@ -44,31 +54,169 @@ int main()
     
     F12();  //  robot 
     
-    F1();   // is 
+    F1();
+    F1();
     
-    F1();   // still
+    SQR(); //squareing uop
     
-    F1();   // moving.
     
-    L90(); // robot is approaching final.
+    // moving.
     
-    // those darn glacers keep gettin in the floor boards.
+    
+    
+    
+    
+    // those darn glaciers keep gettin in the floor boards.
+    
+    
+   B6(); //backing uop after squaring
+   
+   L90();
+    
+     // robot is approaching final.
+    
+   motor(0,-25);
+   motor(1,25);  // my name is dan
+   msleep(50);
     
     F6();
+	F1();
+    F1();
     
-    set_servo_position(3, 750);
-    msleep(500);
-
-    SC(); // Close Claw
-	
-    enable_servos();
-	set_servo_position(2, 618); // BRO JACOB KEEPS SAYING DAN, my names DAN and i am DAN i would weally wike to see a DAN. 
-    msleep(500);
+    ao();
     
-    set_servo_position(3, 1200);
     msleep(1000);
     
-    printf(" yer a nerd ");
+    motor(1,0);
+    motor(0,25);
+    msleep(50);
+    
+    motor(1,-10);
+    motor(0,-10);
+    msleep(50);
+    
+    
+    ao();
+        
+    enable_servos();
+	set_servo_position(2, 450); //Grab 
+    msleep(500);
+    
+    
+    set_servo_position(3, 800); 
+    msleep(970);  
+    
+    enable_servos();
+	set_servo_position(2, 600);  
+    msleep(2000);
+    
+  
+    
+    
+    ao();
+    
+    msleep(500);
+    
+    set_servo_position(2, 650);
+    
+    msleep(500);
+    
+    set_servo_position(2, 550);
+   
+    msleep(500);
+    
+    set_servo_position(2, 400);    
+
+    msleep(500);
+    
+	set_servo_position(2, 450);
+   
+    msleep(500);
+    
+    set_servo_position(2, 300);
+    
+    msleep(500);
+    
+    set_servo_position(3, 950);// Highten Arm
+    msleep(500);
+    
+    set_servo_position(2, 625);
+    msleep(500);
+    
+    set_servo_position(3, 750);// Lower Arm
+    msleep(500);
+    
+    set_servo_position(2, 650);
+    msleep(500);
+
+    set_servo_position(2, 700);
+    msleep(500);
+    
+    motor(1,100);
+    motor(0,-100);// shake it off 
+    msleep(750);
+    
+    motor(1,-100);
+    motor(0,100); // shake it off 
+    msleep(500);
+    
+    motor(1,100);
+    motor(0,-100);// ah ah
+    msleep(750);
+    
+    motor(1,-100);
+    motor(0,100);
+    msleep(500);
+    
+    motor(1,100);
+    motor(0,-100);
+    msleep(750);
+    
+    motor(1,-100);
+    motor(0,100);
+    msleep(500);
+    
+    motor(1,100);
+    motor(0,-100);
+    msleep(750);
+    
+    motor(1,-100);
+    motor(0,100);
+    msleep(400);
+
+    motor(1,100);
+    motor(0,-100);
+    msleep(300);
+    
+    motor(1,-100);
+    motor(0,100);
+    msleep(200);
+    
+    enable_servos();
+    set_servo_position(3,1500);
+    
+    L90();
+   
+    motor(1,30);
+    motor(0,25);
+    msleep(1000);
+
+    msleep(500);
+    
+    SQR();
+    
+    B6();
+    
+    L90();
+    
+    SQR();
+    
+    B6();
+    
+    
+    // insert roblox oof noise
+	 
+    printf(" Ghuoae    meat tastes good! "); //meat sack
     
     
     return 0;
