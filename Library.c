0a544e7848d0b49af8e059a371ab5a44eb8b6afa
GriffinsBotball <99513783+GriffinsBotball@users.noreply.github.com>
2022-03-25 16:28:52 -0500

    Update Library.c(Roomba)

--- Library.c
+++ Library.c
@@ -1,5 +1,6 @@
 #include "Library.h"
 #include <kipr/wombat.h>
+#include <sys/time.h>
 
 void Right_Turn_Roomba()
 {
@@ -24,6 +25,7 @@ void Bump_Square_Up()
         create_drive_direct(100,100);
     }
 }
+
 void Insult()
 {
     printf("You Bad\\n");
@@ -34,8 +36,45 @@ void Compliment()
 }
 void Button_Square_Up()
 {
-    while(digital(8) == 0 || digital(9) == 0)
+    while(digital(1) == 0 || digital(0) == 0)
     {
-        create_drive_direct(100,100);
+        create_drive_direct(30,30);
+    }
+}
+
+
+void Half_Right_Turn_Roomba()
+{
+	set_create_total_angle(0);
+    while( get_create_total_angle() >= -45 )
+	{
+		create_drive_direct(100,-100);
+	}
+}
+void Half_Left_Turn_Roomba()
+{
+	set_create_total_angle(0);    
+    while( get_create_total_angle() <= 45 )
+	{
+		create_drive_direct(-100,100);
+	}
+}
+void line_follow(int tophat_port, int threshold, int milliseconds) {
+    struct timeval start, stop;
+    gettimeofday(&start, NULL);
+    gettimeofday(&stop, NULL);
+    int diff_ms = (stop.tv_sec - start.tv_sec)*1000 + (stop.tv_usec - start.tv_usec)/1000;
+    
+    while(diff_ms<=milliseconds){
+        while(analog(tophat_port) < threshold && diff_ms<=milliseconds){
+            create_drive_direct(100,0);
+            gettimeofday(&stop, NULL);
+        	diff_ms = (stop.tv_sec - start.tv_sec)*1000 + (stop.tv_usec - start.tv_usec)/1000;
+        }
+        while(analog(tophat_port) > threshold  && diff_ms<=milliseconds){
+            create_drive_direct(0,100);
+            gettimeofday(&stop, NULL);
+        	diff_ms = (stop.tv_sec - start.tv_sec)*1000 + (stop.tv_usec - start.tv_usec)/1000;
+        }
     }
 }
